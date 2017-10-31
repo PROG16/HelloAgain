835a4d2f774ae980ec35ef10b14f1cb573fba6ee
Dewwlo <a.bjrnlund@gmail.com>
2017-10-31 10:59:29 +0100

    Andreas added new speak function.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace HelloAgain
         static void Main(string[] args)
         {
             Console.WriteLine(MarcusHultkrantz.Speak());
+            AndreasBjörnlund.Speak();
+            Console.ReadLine();
         }
     }
 }
