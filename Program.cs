f3248dad06c72406d5d61dd139fe8d430cad496f
Call3z <electrodez@gmail.com>
2017-10-31 11:23:26 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -12,17 +12,11 @@ namespace HelloAgain
         {
             FredrikHaglund.Speak();
             Console.WriteLine(MarcusHultkrantz.Speak());
-<<<<<<< HEAD
             Console.WriteLine(Khalid.Speak());
-=======
             AndreasBjörnlund.Speak();
-<<<<<<< HEAD
->>>>>>> a11dfecc04b02dff56f9d68586c07bd4bc586886
-=======
             ErikJonson.Speak();
->>>>>>> 6e401ca1c452c41a46587757d6b7514127c2637d
-            Console.ReadLine();
             MasterSpeaker();
+            Console.ReadLine();
         }
 
         static void MasterSpeaker()
