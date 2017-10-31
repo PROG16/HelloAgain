ba1f61c3b3ec8594632bccbce0a6f19b53196f7c
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-10-31 11:27:36 +0100

    Hihihihihih

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿namespace HelloAgain
+﻿using System;
+
+namespace HelloAgain
 {
     class Program
     {
         static void Main(string[] args)
         {
             FredrikHaglund.Speak();
-            Console.WriteLine(MarcusHultkrantz.Speak());
+            while (true)
+            {
+                Console.WriteLine(MarcusHultkrantz.Speak());
+            }
             Console.WriteLine(Khalid.Speak());
             AndreasBjörnlund.Speak();
             ErikJonson.Speak();
             MattiasClass.SpeakTo();
             MasterSpeaker();
             Console.ReadLine();
-            Patrik.Speak();
             MasterSpeaker();
         }
 
