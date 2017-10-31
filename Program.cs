1c782cab8158e7f3140f7927f684d38361f73eac
Perly Gustafsson <contact@daperley.com>
2017-10-31 11:16:18 +0100

    Perly speak added

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloAgain
+﻿namespace HelloAgain
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PerlyGustafsson.Speak();
         }
     }
 }
